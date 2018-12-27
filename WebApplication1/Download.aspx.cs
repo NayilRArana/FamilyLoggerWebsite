9a3513537c1378da9bba35890986f4437ee3792b
Nayil <n.arana1@outlook.com>
2018-12-27 13:28:43 -0500

    Installer download enabled

--- WebApplication1/Download.aspx.cs
+++ WebApplication1/Download.aspx.cs
@@ -11,7 +11,7 @@ namespace FamilyLoggerWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Response.Redirect("/FamilyLogger.zip");
         }
     }
 }
